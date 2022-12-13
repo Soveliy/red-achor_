489ef31cbac89746261f77721ba6e122e6e1a9c2
Soveliy <nikita.elagin2012@yandex.ru>
2022-12-13 15:58:23 +0300

    upload

--- src/assets/scripts/script.js
+++ src/assets/scripts/script.js
@@ -28,6 +28,32 @@ document.addEventListener('DOMContentLoaded', () => {
     },
   });
     @@include('./blocks/text.js');
+    $(".header__productionButton").click(function(){
+      $(this).toggleClass("js-active")
+      $(this).next().toggleClass("js-active")
+      $(this).next().next().toggle(500)
+    })
+    $(".bgShadow").click(function(){
+      $(".header__productionButton,.catalog-menu").removeClass("js-active")
+      $(".bgShadow").hide(500)
+    })
   
+    document.addEventListener('keydown', function (e) {
+      if(e.keyCode === 27){
+        $(".header__productionButton,.catalog-menu").removeClass("js-active")
+        $(".bgShadow").hide(500)
+      }
+  });
+    
+    $(function() {
+    
+      $('ul.catalog-menu__main-list').on('click', 'li:not(.js-active)', function() {
+       
+        $(this)
+          .addClass('js-active').siblings().removeClass('js-active')
+          .closest('div.catalog-menu__row').find('div.catalog-menu__item-right').removeClass('js-active').eq($(this).index()).addClass('js-active');
+      });
+      
+    });
 
 });
